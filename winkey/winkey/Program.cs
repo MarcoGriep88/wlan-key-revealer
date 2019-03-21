0c023e0fe0c943d8cc90053e205f603f6fd19241
Griep <Griep.Marco@BB.INT>
2019-03-21 09:11:53 +0100

    Initial Commit

---
+++ winkey/winkey/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace winkey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Revealer revealer = new Revealer();
+        }
+    }
+    public class Revealer
+    {
+        List<String> keys = new List<String>();
+        List<String> wlans = new List<String>();
+
+        public Revealer()
+        {
+            keys.Clear();
+            Init();
+        }
+
+        private void Init()
+        {
+            ReadWLAN();
+            GetWLANKeys();
+        }
+
+
+        private void GetWLANKeys()
+        {
+            foreach(string wlan in wlans)
+            {
+                //Console.WriteLine(wlan);
+                string key = GetKeyForWLAN(wlan);
+                Console.WriteLine(wlan + " : " + key);
+            }
+        }
+
+        private string GetKeyForWLAN(string wlan)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.Arguments = "wlan show profile name=\"" + wlan + "\" key=clear";
+            psi.FileName = "netsh";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            Process p = new Process();
+            p.StartInfo = psi;
+
+            p.Start();
+            //p.WaitForExit();
+
+            Thread.Sleep(1000);
+
+            string val = String.Empty;
+            StreamReader reader = p.StandardOutput;
+            while (reader.Peek() >= 0)
+            {
+                string ln = reader.ReadLine();
+                if (ln.Contains("Schlüsselinhalt") || ln.Contains("Key Content"))
+                {
+                    val = ln.Split(':')[1];
+                    break;
+                }
+            }
+            return val;
+        }
+
+        private void ReadWLAN()
+        {
+            ProcessStartInfo psi = new ProcessStartInfo();
+            psi.Arguments = "wlan show profiles";
+            psi.FileName = "netsh";
+            psi.UseShellExecute = false;
+            psi.RedirectStandardOutput = true;
+
+            Process p = new Process();
+            p.StartInfo = psi;
+
+            p.Start();
+            //p.WaitForExit();
+
+            Thread.Sleep(1000);
+
+            StreamReader reader = p.StandardOutput;
+            bool started = false;
+            while (reader.Peek() >= 0)
+            {
+                string ln = reader.ReadLine();
+                if (ln == "---------------")
+                    started = true;
+
+                if (started)
+                {
+                    try { wlans.Add(ln.Split(':')[1].Trim());}
+                    catch {}
+                }
+            }
+        }
+    }
+}
---
+++ winkey/winkey/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("winkey")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Please Enter")]
+[assembly: AssemblyProduct("winkey")]
+[assembly: AssemblyCopyright("Copyright © Please Enter 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("902347f7-500b-4cc9-8df0-222f03ad0eda")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
